4e9275c533298115aa2add2a6a7418f4428cd67d
Alejandro Urroz <alejandrourroz86@gmail.com>
2021-11-11 10:16:19 +0100

    updated proyect hackyourweather

---
+++ project/hackyourweather/src/secret.js
@@ -0,0 +1,11 @@
+const APIKEY = process.env.REACT_APP_OPENWEATHERMAP_API_KEY;
+
+export default APIKEY;
+
+//- Make an API call when the button is clicked
+//- Display the city + weather information similar to week 1
+//- Make a component for the search
+//- Incorporate error handling for the API call
+//- Show user feedback when there are no cities searched for yet
+//- All your components should be tested
+//- Redeploy your static site
